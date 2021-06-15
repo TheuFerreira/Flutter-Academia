d487d670d260959bdee2297f98d0301e3d02792f
Matheus Ferreira <matheus.44300@gmail.com>
2021-06-15 11:29:16 -0300

    Removed Staggered Grid View

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_academia/app/features/home/home_controller.dart';
 import 'package:flutter_academia/model/connection.dart';
 import 'package:flutter_academia/app/features/home/components/tile_component.dart';
 import 'package:flutter_academia/app/features/home/models/day_model.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -160,31 +159,3 @@ class _HomePageState extends State<HomePage> {
   void _onTap(DayModel dayModel) async =>
       await Navigator.pushNamed(context, '/day', arguments: dayModel);
 }
-
-/*return new WillPopScope(
-      child: new Scaffold(
-        body: new StaggeredGridView.count(
-          primary: false,
-          crossAxisCount: 4,
-          staggeredTiles: const <StaggeredTile>[
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(2, 1.75),
-            StaggeredTile.count(4, 1.20),
-          ],
-          children: controller.days.map((e) {
-            return new TileComponent(
-              day: e,
-              onTap: _onTap,
-            );
-          }).toList(),
-        ),
-      ),
-      onWillPop: () async {
-        Connection.db.close = true;
-        return true;
-      },
-    );*/
